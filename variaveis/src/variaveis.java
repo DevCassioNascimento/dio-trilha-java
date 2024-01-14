ea7beeec8493ae9b8ed7fde11b55d698eb94faf1
Cassio <cassionascimentovieira@outlook.com>
2024-01-14 17:49:47 -0300

    aritmética/if/ternario/

--- variaveis/src/variaveis.java
+++ variaveis/src/variaveis.java
@@ -1,5 +1,63 @@
 public class variaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String nome = "Cassio";
+        char primeiraLetra = 'c';
+        byte idade = 123;
+        short ano = 2024;
+        int cep = 333333333; // se começar com 0 necessario mudar para String 
+        long inscritos = 98765432109l; // se começar com 0 necessario mudar para String 
+        float pi = 3.14f; 
+        double salario = 1275.33; 
+
+
+       /*  
+        // classe Aritmético 
+
+        double soma = 10.5 + 14.7;
+        int subtra = 300 - 200;
+        int multi = 15 * 5;
+        int dividido = 20 * 2 ; 
+        int resto = 40 % 2; 
+        double mix = (10/2) + (5 * 2);
+        */
+
+
+        
+         /* 
+
+        // tornando um numero negativo em possitivo 
+
+        int numero = -10; 
+        numero = numero * -1; 
+        System.out.println(numero); */
+
+
+        /* 
+        // if /else = se/se nao 
+        int notaDoAluno = 7;
+        int media = 5;
+        String resultado = "";
+
+        if(notaDoAluno >= media) {
+            resultado = "passou";
+        }else{
+            resultado = "reprovou";
+        }
+
+        System.out.println(resultado);
+         */
+
+
+
+          /* 
+         //ternario, forma mais rapida da condição if 
+
+         int notaDoAluno = 7;
+         int media = 5;
+         String resultado = notaDoAluno >= media ? "passou" : "reprovou"; 
+         System.out.println(resultado);
+         */
+
+
     }
 }
